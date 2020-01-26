c1a0716a983692996ce564fafe915ce6d46045c5
Evnen Ovchinnikov <ow4innikow@gmail.com>
2020-01-26 22:51:45 +0200

    тяжелое задание 02 готово

--- lesson02up/script.js
+++ lesson02up/script.js
@@ -3,12 +3,12 @@ let num = 266219;
 let str = num.toString();
 let arr = str.split('');
 let multi = 1;
-for (let i = 0; i < arr.length; i++){
+for (let i = 0; i < arr.length; i++) {
   multi *= (+arr[i]);
-  };
+  }
 console.log(multi);
 
-multi3= multi ** 3;
+let multi3= multi * multi * multi;
 
 str = multi3.toString();
 arr = str.split('');
